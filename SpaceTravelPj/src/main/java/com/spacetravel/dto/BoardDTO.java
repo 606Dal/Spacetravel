d3791b79c77cfee97911a50d29813f518729e880
Misyel <ydw317@naver.com>
2024-09-23 17:25:00 +0900

    확인 중

--- SpaceTravelPj/src/main/java/com/spacetravel/dto/BoardDTO.java
+++ SpaceTravelPj/src/main/java/com/spacetravel/dto/BoardDTO.java
@@ -9,77 +9,90 @@ import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
 public class BoardDTO {
-	
+
 	private int id;
-	
+
 	@NotEmpty(message = "제목을 입력해 주세요.")
 	@Size(max = 50)
 	private String subject;
-	
+
 	@NotEmpty(message = "내용을 입력해 주세요.")
 	private String content;
-	
+
 	private String writer;
 	private LocalDateTime regdate;
 	private LocalDateTime updatedAt;
-	
+
 	@PositiveOrZero
 	private int hit;
-	
+
 	// getter, setter
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getSubject() {
 		return subject;
 	}
+
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public String getWriter() {
 		return writer;
 	}
+
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
+
 	public LocalDateTime getRegdate() {
 		return regdate;
 	}
+
 	public void setRegdate(LocalDateTime regdate) {
 		ZoneId utcZone = ZoneId.of("UTC");
-        ZoneId koreaZone = ZoneId.of("Asia/Seoul");
-        
-        ZonedDateTime utcTime = regdate.atZone(utcZone);
-        ZonedDateTime koreaTime = utcTime.withZoneSameInstant(koreaZone);
-        
+		ZoneId koreaZone = ZoneId.of("Asia/Seoul");
+
+		ZonedDateTime utcTime = regdate.atZone(utcZone);
+		ZonedDateTime koreaTime = utcTime.withZoneSameInstant(koreaZone);
+
 		this.regdate = koreaTime.toLocalDateTime();
 	}
+
 	public LocalDateTime getUpdatedAt() {
 		return updatedAt;
 	}
+
 	public void setUpdatedAt(LocalDateTime updatedAt) {
 		this.updatedAt = updatedAt;
 	}
+
 	public int getHit() {
 		return hit;
 	}
+
 	public void setHit(int hit) {
 		this.hit = hit;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "BoardDTO [id=" + id + ", subject=" + subject + ", content=" + content + ", writer=" + writer
 				+ ", regdate=" + regdate + ", updatedAt=" + updatedAt + ", hit=" + hit + "]";
 	}
-	
+
 }
