47c98cf7ed6d02965c632dbdc0ca635bf01bce90
Misyel <ydw317@naver.com>
2024-09-23 17:01:16 +0900

    확인 중

--- SpaceTravelPj/src/main/java/com/spacetravel/controller/ReplyController.java
+++ SpaceTravelPj/src/main/java/com/spacetravel/controller/ReplyController.java
@@ -24,23 +24,23 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/replies")
 public class ReplyController {
-	
+
 	@Autowired
 	private ReplyService replyService;
-	
+
 	private static final Logger log = LoggerFactory.getLogger(BoardController.class);
-	
+
 	// 댓글 추가
 	@PostMapping("/writeReply")
 	public ResponseEntity<String> writeReply(@Valid @RequestBody ReplyDTO replyDTO,
-			Authentication authentication){
-		
+			Authentication authentication) {
+
 		ResponseEntity<String> resEntity = null;
-		
+
 		try {
 			replyDTO.setReplier(authentication.getName());
 			replyService.insertReply(replyDTO);
-				
+
 			resEntity = new ResponseEntity<String>("Success", HttpStatus.OK);
 		} catch (Exception e) {
 			log.warn("댓글 추가 중 오류 발생");
@@ -49,55 +49,55 @@ public class ReplyController {
 		return resEntity;
 	}
 
-/*
- * // 댓글 리스트 가져오기
- * 시큐리티 처리 때문에 boardreadPage에서 목록 출력함
- */
-  
-    // 댓글 수정하기
-    @PutMapping("/{replyid}")
-    public ResponseEntity<String> modifyReply(@PathVariable("replyid") Integer replyid,
-	        @RequestBody ReplyDTO replyDTO,
-	        @AuthenticationPrincipal CustomUserDetails userDetails){
-		
-    	ResponseEntity<String> resEntity = null;
-    	String replier = replyDTO.getReplier();
-    	String userRole = userDetails.getUser().getRolename();
-    	String username = userDetails.getUser().getUsername();
-		
-    	try {
-    		if(replier.equals(username) || userRole.equals("ADMIN")) {
-    			replyDTO.setReplyid(replyid);
-    			replyService.modifyReply(replyDTO);
-    			resEntity = new ResponseEntity<String>("Success", HttpStatus.OK);
-    		}
-    	} catch (Exception e) {
-    		log.warn("댓글 수정 중 오류 발생");
-    	}
-    	return resEntity;
+	/*
+	 * // 댓글 리스트 가져오기
+	 * 시큐리티 처리 때문에 boardreadPage에서 목록 출력함
+	 */
+
+	// 댓글 수정하기
+	@PutMapping("/{replyid}")
+	public ResponseEntity<String> modifyReply(@PathVariable("replyid") Integer replyid,
+			@RequestBody ReplyDTO replyDTO,
+			@AuthenticationPrincipal CustomUserDetails userDetails) {
+
+		ResponseEntity<String> resEntity = null;
+		String replier = replyDTO.getReplier();
+		String userRole = userDetails.getUser().getRolename();
+		String username = userDetails.getUser().getUsername();
+
+		try {
+			if (replier.equals(username) || userRole.equals("ADMIN")) {
+				replyDTO.setReplyid(replyid);
+				replyService.modifyReply(replyDTO);
+				resEntity = new ResponseEntity<String>("Success", HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			log.warn("댓글 수정 중 오류 발생");
+		}
+		return resEntity;
 	}
-    
-    // 댓글 삭제하기
-    @DeleteMapping("/{replyid}")
-    public ResponseEntity<String> deleteReply(@PathVariable("replyid") Integer replyid,
-    		@RequestBody ReplyDTO replyDTO, 
-    		@AuthenticationPrincipal CustomUserDetails userDetails){
-    	
-    	ResponseEntity<String> resEntity = null;
-    	String replier = replyDTO.getReplier();
-    	String userRole = userDetails.getUser().getRolename();
-    	String username = userDetails.getUser().getUsername();
-    	
-    	try {
-    		if(replier.equals(username) || userRole.equals("ADMIN")) {
-    			replyDTO.setReplyid(replyid);
-    			replyService.deleteReply(replyid);
-    			resEntity = new ResponseEntity<String>("Success", HttpStatus.OK);
-    		}
-    	} catch (Exception e) {
-    		log.warn("댓글 삭제 중 오류 발생");
-    	}
-    	return resEntity;
-    }
-    
+
+	// 댓글 삭제하기
+	@DeleteMapping("/{replyid}")
+	public ResponseEntity<String> deleteReply(@PathVariable("replyid") Integer replyid,
+			@RequestBody ReplyDTO replyDTO,
+			@AuthenticationPrincipal CustomUserDetails userDetails) {
+
+		ResponseEntity<String> resEntity = null;
+		String replier = replyDTO.getReplier();
+		String userRole = userDetails.getUser().getRolename();
+		String username = userDetails.getUser().getUsername();
+
+		try {
+			if (replier.equals(username) || userRole.equals("ADMIN")) {
+				replyDTO.setReplyid(replyid);
+				replyService.deleteReply(replyid);
+				resEntity = new ResponseEntity<String>("Success", HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			log.warn("댓글 삭제 중 오류 발생");
+		}
+		return resEntity;
+	}
+
 }
